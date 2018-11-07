b8e8107748307baeea36e9a9f0dcb734f2b9d5ae
Miha04 <37040019+Miha04@users.noreply.github.com>
2018-11-07 20:42:12 +0200

    Add files via upload

---
+++ BusinessLayer/CityRepository.cs
@@ -0,0 +1,33 @@
+﻿using DataLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusinessLayer
+{
+    public class CityRepository : ICityRepository
+    {
+        private CityContext _context;
+
+        //private readonly CityContext _context;
+
+        public CityRepository(CityContext context)
+        {
+            _context = context;
+        }
+        public void Create(City city)
+        {
+            _context.Cities.Add(city);
+            _context.SaveChanges();
+        }
+        public City GetByld(Guid id)
+        {
+            return _context.Cities.Find(id);
+        }
+        public IReadOnlyList<City>GetAll()
+        {
+            return _context.Cities.ToList();
+        }
+    }
+}
---
+++ BusinessLayer/ICityRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public interface ICityRepository
+    {
+        void Create(City city);
+        IReadOnlyList<City> GetAll();
+        City GetByld(Guid id);
+    }
+}
