3c457a72b754f811df4de88a6abc933927bff140
Miha04 <37040019+Miha04@users.noreply.github.com>
2018-11-07 20:49:42 +0200

    Add files via upload

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer;
+using CityApplication.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CityApplication.Controllers
+{
+    public class CitiesController : Controller
+    {
+        private readonly ICityRepository _repository;
+
+        public CitiesController(ICityRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            var cities = _repository.GetAll();
+            var citiesVM = new List<CityViewModel>();
+
+            foreach (var city in cities)
+            {
+                citiesVM.Add(new CityViewModel
+                {
+                    Description = city.Description
+                });
+            }
+            return View(citiesVM);
+        }
+    }
+}
---
+++ ViewModel/CityViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityApplication.ViewModel
+{
+    public class CityViewModel
+    {
+        [Required]
+        public string Description { get; set; }
+    }
+}
