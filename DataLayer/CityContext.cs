c43fbb879bd776d46e4779ffaa516b9a483c7894
Miha04 <37040019+Miha04@users.noreply.github.com>
2018-11-07 20:43:21 +0200

    Add files via upload

---
+++ DataLayer/CityContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public sealed class CityContext : DbContext
+    {
+        public CityContext(DbContextOptions<CityContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<City> Cities { get; set; }
+    }
+}
